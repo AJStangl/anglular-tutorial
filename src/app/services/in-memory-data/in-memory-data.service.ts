3b13ea4bc696ed2fc2fa38a40e0f72cbbce4ff9a
AJ Stangl <ajstangl@gmail.com>
2020-12-05 21:56:25 -0500

    'foo'

--- src/app/services/in-memory-data/in-memory-data.service.ts
+++ src/app/services/in-memory-data/in-memory-data.service.ts
@@ -7,10 +7,21 @@ import {HEROES} from '../../models/mock-heroes';
   providedIn: 'root',
 })
 export class InMemoryDataService implements InMemoryDbService {
-  createDb(): { HEROES: Hero[] } {
-    return {HEROES};
+  createDb(): { heroes: Hero[] } {
+    const heroes = [
+      { id: 11, name: 'Dr Nice' },
+      { id: 12, name: 'Narco' },
+      { id: 13, name: 'Bombasto' },
+      { id: 14, name: 'Celeritas' },
+      { id: 15, name: 'Magneta' },
+      { id: 16, name: 'RubberMan' },
+      { id: 17, name: 'Dynama' },
+      { id: 18, name: 'Dr IQ' },
+      { id: 19, name: 'Magma' },
+      { id: 20, name: 'Tornado' }
+    ];
+    return {heroes};
   }
-
   // Overrides the genId method to ensure that a hero always has an id.
   // If the heroes array is empty,
   // the method below returns the initial number (11).
